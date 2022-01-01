a5b6968c19ea75a5897df8f4bf87c8797e4653f5
Nathan Kitchen <nkitchen@arista.com>
2022-01-01 11:31:07 -0800

    Day 5, part 1

--- day05a.py
+++ day05a.py
@@ -14,42 +14,9 @@ def main():
     program = [int(x) for x in next(f).split(',')]
     inputs = [1]
 
-    c = intcode.Computer()
-    outputs = c.run(program, inputs)
+    p = intcode.Process(program, inputs)
+    outputs = p.run()
     for y in outputs:
         print(y)
-
-class Computer():
-    ADD = 1
-    MUL = 2
-    HALT = 99
-
-    def __init__(self, program):
-        self.mem = program[:]
-
-    def run(self):
-        pos = 0
-
-        if DEBUG:
-            print(f"{pos} ", end='')
-
-        while True:
-            opcode = self.mem[pos]
-            if opcode == self.ADD:
-                pa, pb, pc = self.mem[pos + 1:pos + 4]
-                r = self.mem[pa] + self.mem[pb]
-                self.mem[pc] = r
-            elif opcode == self.MUL:
-                pa, pb, pc = self.mem[pos + 1:pos + 4]
-                r = self.mem[pa] * self.mem[pb]
-                self.mem[pc] = r
-            elif opcode == self.HALT:
-                break
-            else:
-                raise Exception(f"Unknown opcode: {opcode}")
-            pos += 4
-
-            if DEBUG:
-                pprint(self.mem)
 
 main()
--- intcode.py
+++ intcode.py
@@ -1,138 +1,189 @@
 import operator
 import os
+import sys
 from collections import namedtuple
+from enum import IntEnum
 
 DEBUG = os.environ.get("DEBUG")
 
-ADD = 1
-MUL = 2
-INPUT = 3
-OUTPUT = 4
-HALT = 99
+class Op(IntEnum):
+    ADD = 1
+    MUL = 2
+    INPUT = 3
+    OUTPUT = 4
+    HALT = 99
 
-Inst = namedtuple('Inst', ['in_param_fns', 'op_fn', 'out_param_fns', 'ip_incr'])
+class Mode(IntEnum):
+    ADDR = 0
+    IMM = 1
 
-class Computer():
-    def run(self, program, inputs):
+N_ARGS = {
+    Op.ADD: 2,
+    Op.MUL: 2,
+    Op.INPUT: 0,
+    Op.OUTPUT: 1,
+}
+
+class Process():
+    def __init__(self, program, inputs):
         self.mem = list(program)
         self.inputs = inputs
+
+    def run(self):
         self.outputs = []
 
-        ip = 0
-        while True:
-            inst = self.decode(ip)
+        self.ip = 0
+        if DEBUG:
+            self.dump()
 
-            in_param_fns = [f() for f in inst.in_param_fns]
+        while True:
+            inst = self.decode(self.ip)
+
+            args = [self.load_arg(p) for p in inst.src_params]
             try:
-                out_param = inst.op_fn(*in_param_fns)
-                out_param_fns = [out_param]
+                ret_values = self.exec(inst.op, args)
             except StopIteration:
                 break
-            for f, y in zip(inst.out_param_fns, out_param_fns):
-                f(y)
 
-            ip += inst.ip_incr
+            for rv, p in zip(ret_values, inst.dst_params):
+                self.store(rv, p)
+
+            self.ip += inst.ip_incr()
 
             if DEBUG:
-                print(self.mem)
+                self.dump()
 
         return self.outputs
-
-    ADDR_MODE = 0
-    IMM_MODE = 1
-
-    N_ARGS = {
-        ADD: 2,
-        MUL: 2,
-        INPUT: 0,
-        OUTPUT: 1,
-    }
 
     def decode(self, ip):
         code = self.mem[ip]
 
         op = code % 100
+        try:
+            op = Op(op)
+        except ValueError:
+            pass
         code //= 100
 
-        n_args = self.N_ARGS.get(op, 0)
+        n_args = N_ARGS.get(op, 0)
 
         modes = []
         for i in range(n_args):
             modes.append(code % 10)
             code //= 10
 
-        if op == ADD:
+        # Unrecognized mode?  Keep it undecoded.
+        if not(set(modes) <= set([Mode.ADDR, Mode.IMM])):
+            op = code
+
+        if op in [Op.ADD, Op.MUL]:
             x = self.mem[ip + 1]
             y = self.mem[ip + 2]
             z = self.mem[ip + 3]
-            in_param_fns = [
-                self.arg_fn(x, modes[0]),
-                self.arg_fn(y, modes[1]),
+            src_params = [
+                Param(x, modes[0]),
+                Param(y, modes[1]),
             ]
-            op_fn = operator.add
-            out_param_fns = [
-                self.out_fn(z),
+            dst_params = [
+                Param(z, Mode.ADDR),
             ]
-        elif op == MUL:
-            x = self.mem[ip + 1]
-            y = self.mem[ip + 2]
-            z = self.mem[ip + 3]
-            in_param_fns = [
-                self.arg_fn(x, modes[0]),
-                self.arg_fn(y, modes[1]),
-            ]
-            op_fn = operator.mul
-            out_param_fns = [
-                self.out_fn(z),
-            ]
-        elif op == INPUT:
+        elif op == Op.INPUT:
             z = self.mem[ip + 1]
-            in_param_fns = []
-            op_fn = self.read_input
-            out_param_fns = [
-                self.out_fn(z),
+            src_params = []
+            dst_params = [
+                Param(z, Mode.ADDR),
             ]
-        elif op == OUTPUT:
+        elif op == Op.OUTPUT:
             x = self.mem[ip + 1]
-            in_param_fns = [
-                self.arg_fn(x, modes[0]),
+            src_params = [
+                Param(x, modes[0]),
             ]
-            op_fn = lambda x: x
-            out_param_fns = [
-                self.write_output,
-            ]
-        elif op == HALT:
-            in_param_fns = []
-            out_param_fns = []
-            def f():
-                raise StopIteration
-            op_fn = f
+            dst_params = []
+        else:
+            src_params = []
+            dst_params = []
+
+        return Inst(op, src_params, dst_params)
+
+    def load_arg(self, param):
+        if param.mode == Mode.IMM:
+            return param.num
+        else:
+            assert param.mode == Mode.ADDR
+            return self.mem[param.num]
+
+    def store(self, value, param):
+        assert param.mode == Mode.ADDR
+        self.mem[param.num] = value
+
+    def exec(self, op, args):
+        r = None
+        if op == Op.ADD:
+            r = args[0] + args[1]
+        elif op == Op.MUL:
+            r = args[0] * args[1]
+        elif op == Op.INPUT:
+            r = self.inputs[0]
+            self.inputs = self.inputs[1:]
+        elif op == Op.OUTPUT:
+            self.outputs.append(args[0])
+        elif op == Op.HALT:
+            raise StopIteration
         else:
             raise Exception(f"Unknown opcode: {op}")
 
-        k = len(in_param_fns) + 2
-        return Inst(in_param_fns, op_fn, out_param_fns, k)
+        ret_values = []
+        if r is not None:
+            ret_values.append(r)
+        return ret_values
 
-    def arg_fn(self, a, mode):
-        if mode == self.ADDR_MODE:
-            def f():
-                return self.mem[a]
-            return f
-        elif mode == self.IMM_MODE:
-            def f():
-                return a
-            return f
-        raise Exception(f"Unknown mode: {mode}")
+    def dump(self):
+        def write(s):
+            print(s, file=sys.stderr, end='')
 
-    def out_fn(self, a):
-        def f(v):
-            self.mem[a] = v
-        return f
+        write(f'< {self.inputs}\n')
+        write(f'> {self.outputs}\n')
 
-    def read_input(self):
-        x = self.inputs[0]
-        self.inputs = self.inputs[1:]
-        return x
+        ip = 0
+        while ip < len(self.mem):
+            if ip == self.ip:
+                write("==> ")
+            else:
+                write("    ")
 
-    def write_output(self, x):
-        self.outputs.append(x)
+            write(f" [{ip:0>3d}] ")
+
+            inst = self.decode(ip)
+
+            # 20 columns for 1-4 memory values
+            m = ""
+            for i in range(ip, ip + inst.ip_incr()):
+                m += f"{self.mem[i]:4d} "
+            write(f"{m:20}")
+
+            write(str(inst) + "\n")
+
+            ip += inst.ip_incr()
+        write("\n")
+
+class Param(namedtuple('Param', 'num mode')):
+    def __str__(self):
+        if self.mode == Mode.ADDR:
+            return f"[{self.num}]"
+        else:
+            assert self.mode == Mode.IMM
+            return f"#{self.num}"
+
+class Inst(namedtuple('Inst', ['op', 'src_params', 'dst_params'])):
+    def __str__(self):
+        if isinstance(self.op, Op):
+            op = self.op.name
+        else:
+            op = f"{self.op}!"
+        s = op + ' ' + ', '.join(str(a) for a in self.src_params)
+        if self.dst_params:
+            s += ' > ' + ', '.join(str(r) for r in self.dst_params)
+        return s
+
+    def ip_incr(self):
+        return 1 + len(self.src_params) + len(self.dst_params)
