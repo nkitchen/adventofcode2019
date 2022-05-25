73f8354437e66e44645d90aabe3cfaa2ed2e8927
Nathan Kitchen <nkitchen@arista.com>
2022-05-24 19:55:03 -0700

    Day 23, part 2

--- day23b.py
+++ day23b.py
@@ -19,12 +19,12 @@ def dprint(*args, **kwargs):
         print(*args, **kwargs, file=sys.stderr)
 
 Packet = namedtuple("Packet", "addr x y")
+QueueStatus = namedtuple("QueueStatus", "addr idle")
 
 def main():
     f = fileinput.input()
     program = [int(x) for x in next(f).split(',')]
 
-    network = mp.Queue()
     nat_queue = mp.Queue()
 
     n = int(os.environ.get("N", 50))
@@ -40,7 +40,14 @@ def main():
             a = node.read_out()
             x = node.read_out()
             y = node.read_out()
-            network.put(Packet(a, x, y))
+            pkt = Packet(a, x, y)
+
+            dprint(f"Sending {pkt}")
+            if pkt.addr == 255:
+                nat_queue.put(pkt)
+            else:
+                nodes[pkt.addr].write_packet(pkt)
+                nat_queue.put(QueueStatus(pkt.addr, False))
 
     procs = []
     for node in nodes:
@@ -48,49 +55,35 @@ def main():
         procs.append(p)
         p.start()
 
-    def dispatch():
-        while True:
-            pkt = network.get()
-            dprint(f"Dispatched: {pkt}")
-            if pkt.addr == 255:
-                nat_queue.put(pkt)
-            else:
-                nodes[pkt.addr].write_packet(pkt)
-
-    dispatcher = mp.Process(target=dispatch, daemon=True)
-    dispatcher.start()
-
     def run_nat():
         last_packet = None
-        num_idle = 0
         sent_y = set()
+        idlers = set()
 
         while True:
-            msg = nat_queue.get()
-            dprint(f"NAT received: {msg}")
-            if isinstance(msg, Packet):
-                last_packet = msg
-            elif msg >= 0:
-                num_idle += msg
-                dprint(f"NAT {num_idle=}")
-                if num_idle == n:
-                    import time
-                    time.sleep(20)
-                    sys.exit(0)
-                    if network.qsize() > 0:
-                        # Retry
-                        nat_queue.put(0)
+            try:
+                msg = nat_queue.get(timeout=0.010)
+                dprint(f"NAT received: {msg}")
+                if isinstance(msg, Packet):
+                    last_packet = msg
+                else:
+                    assert isinstance(msg, QueueStatus)
+                    if msg.idle:
+                        idlers.add(msg.addr)
                     else:
-                        dprint(f"Network is idle {network.qsize()}.  Sending {last_packet}")
-                        nodes[0].write_packet(last_packet)
-                        num_idle = 0
+                        idlers.discard(msg.addr)
+                    dprint(f"idlers={sorted(idlers)}")
+            except queue.Empty:
+                if len(idlers) == n:
+                    dprint(f"Network is idle.  Sending {last_packet}")
+                    nodes[0].write_packet(last_packet)
+                    idlers.discard(0)
 
-                        if last_packet.y in sent_y:
-                            print(last_packet.y)
-                            return
-                        sent_y.add(last_packet.y)
-            elif msg < 0:
-                num_idle -= 1
+                    if last_packet.y in sent_y:
+                        print(last_packet.y)
+                        return
+                    sent_y.add(last_packet.y)
+
     nat = mp.Process(target=run_nat)
     nat.start()
     nat.join()
@@ -111,12 +104,12 @@ class Node(intcode.Process):
             pkt = self.queue.get(timeout=0.001)
             self._inputs.append(pkt.y)
             if self.idle:
-                self.nat_queue.put(-1)
+                self.nat_queue.put(QueueStatus(self.addr, False))
                 self.idle = False
             return pkt.x
         except queue.Empty:
             if not self.idle:
-                self.nat_queue.put(1)
+                self.nat_queue.put(QueueStatus(self.addr, True))
                 self.idle = True
             return -1
 
